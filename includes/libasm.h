b779a5770f92d51928e9a6063af55aa8893aa85a
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-03-29 17:49:39 +0300

    for pull

--- includes/libasm.h
+++ includes/libasm.h
@@ -2,6 +2,7 @@
 size_t ft_strlen(char *s);
 char *ft_strcpy(char *dst, const char *src);
 int ft_strcmp(char *s1, char *s2);
-ssize_t ft_write(int , char *, size_t);
+ssize_t ft_write(int fields, char *buf, size_t nbyte);
+ssize_t ft_read(int fields, char *buf, size_t nbyte);
 char *ft_strdup(char *s1);
 #include <errno.h>
